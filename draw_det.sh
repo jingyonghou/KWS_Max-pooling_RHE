04ce9e50ef4ac4090fe2b90f655e026c364d09da
Jingyong Hou <jyhou@nwpu-aslp.org>
2020-05-23 14:00:30 +0800

    add results

--- draw_det.sh
+++ draw_det.sh
@@ -1,16 +1,11 @@
-dir=./
-python draw_det.py --x-min=0.0 --x-max=5.0 \
-                   --y-min=1.0 --y-max=8.0 \
-                   --line-style="- - -" \
-                   --color="r b g" \
-                   gru_hixiaowen.png \
-                   "RHM " $dir/test_hixiaowen_0_roc.txt \
+dir=./exp/Ticmini2_Public_ConstraintMaxPooling_random-ngFalse_sa1_ohem200_ratio10_constraint2_ctedge_cl30_cr30_num-p1_num-n1_gru_nl2_hd128_optnoam_bs400_lr0.010_gamma-p0.0_gamma-n0.0_hf0.7_wd5e-5_dp0.5_lc0_rc0_clamp1e-6_seed11
+python draw_det.py --x-min=0.0 --x-max=3.0 \
+                   --y-min=0.0 --y-max=3.0 \
+                   --line-style="- -" \
+                   --color="r b" \
+                   gru_rhe.png \
+                   "RHE Hi Xiaowen " $dir/test_hixiaowen_0_roc.txt \
+                   "RHE NiHao Wenwen " $dir/test_nihaowenwen_0_roc.txt \
 
-python draw_det.py --x-min=0.0 --x-max=5.0 \
-                   --y-min=1.0 --y-max=8.0 \
-                   --line-style="- - -" \
-                   --color="r b g" \
-                   gru_nihaowenwen.png \
-                   "S1" $dir/test_nihaowenwen_0_roc.txt \
 
 

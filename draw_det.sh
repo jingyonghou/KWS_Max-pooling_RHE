96ff9883d7510f9a3b2788dc2674bcfb787cc086
Jingyong Hou <houjingyong@gmail.com>
2020-05-23 16:54:29 +0800

    update

--- draw_det.sh
+++ draw_det.sh
@@ -1,5 +1,5 @@
-dir=./exp/Ticmini2_Public_ConstraintMaxPooling_random-ngFalse_sa1_ohem200_ratio10_constraint2_ctedge_cl30_cr30_num-p1_num-n1_gru_nl2_hd128_optnoam_bs400_lr0.010_gamma-p0.0_gamma-n0.0_hf0.7_wd5e-5_dp0.5_lc0_rc0_clamp1e-6_seed11
-python draw_det.py --x-min=0.0 --x-max=3.0 \
+dir=./
+python draw_det.py --x-min=0.0 --x-max=1.5 \
                    --y-min=0.0 --y-max=3.0 \
                    --line-style="- -" \
                    --color="r b" \
